e15a75d03a117cfb261b3cd78d3a8255657d5a7e
aeter <lucasfernando.dev@gmail.com>
2022-05-22 16:25:57 +0000

    chore: Changing url

--- config/blog.ts
+++ config/blog.ts
@@ -1,7 +1,5 @@
-import previewImage from '/public/assets/preview.png';
-
 export const defaultSiteName = 'Aeter';
 export const defaultTitle = 'Aeter - Um blog sobre desenvolvimento web';
 export const defaultDescription = 'Um blog sobre desenvolvimento web, com publicações diárias de artigos sobre programação, css e bugs.';
-export const defaultThumbnail = previewImage.src ?? '';
+export const defaultThumbnail = '/assets/preview.png' ?? '';
 export const defaultUrl = 'https://blog.lucasfernandodev.com.br';
