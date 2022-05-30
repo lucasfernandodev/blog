45c069ee6151756189b44d4b4935e4dcb84a5ae7
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-05-30 00:51:49 -0300

    fix: fixing flickering image issue when scrolling fast

--- src/components/Utils/Image.tsx
+++ src/components/Utils/Image.tsx
@@ -1,26 +1,23 @@
 import NextImage from 'next/image';
+import { useEffect, useState } from 'react';
 
 interface ImageProps {
   src: string,
   alt: string,
   width?: number,
-  height?: number,
   className?: string | undefined,
 }
 
-const Image = ({src, alt, width, height, className}: ImageProps) => {
-  
+const Image = ({src, alt, width, className}: ImageProps) => {
+
   return(
     <div style={{width: '100%', height: '100%', position: 'relative'}}>
-      <NextImage 
+     <NextImage 
       src={src}
       alt={alt}
       layout='fill'
-      objectFit='cover'
-      width={width}
-      height={height}
+      width={width ? width : '100vw'}
       className={className}
-      priority={true}
     />
     </div>
   )
