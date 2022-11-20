47f19596a7485a5e208843e0636ce79678a6b50d
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-20 14:40:42 -0300

    feat: update legacy props

--- src/components/Utils/Image.tsx
+++ src/components/Utils/Image.tsx
@@ -1,28 +1,40 @@
 import NextImage from 'next/image';
-import {useState } from 'react';
+import {ImageProps} from 'next/image'
+import { HTMLProps, useState } from 'react';
 import imageError from './imageError';
 
-interface ImageProps {
+interface imageProps extends ImageProps {
   src: string,
   alt: string,
   width?: number,
-  className?: string | undefined,
+  height?: number,
+  className?: string,
   priority?: boolean,
 }
 
-const Image = ({src, alt, className,priority = false}: ImageProps) => {
-
+const Image = ({
+  src,
+  alt,
+  width,
+  height,
+  className,
+  priority = false,
+  ...args
+}: imageProps) => {
   const [currentSrc, setCurrentSrc] = useState(src);
 
-  return(
-    <div style={{width: '100%', height: '100%', position: 'relative'}}>
-      <NextImage 
+  return (
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <NextImage
         src={currentSrc}
         alt={alt}
-        layout='fill'
         className={className}
         onError={() => setCurrentSrc(imageError)}
         priority={priority}
+        width={width}
+        height={height}
+        fill
+        {...args}
       />
     </div>
   );
