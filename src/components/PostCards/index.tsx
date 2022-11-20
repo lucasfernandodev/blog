42774ba730730bbf61ec85d05a69169701b825eb
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-20 10:29:10 -0300

    feat: add tranformations images to low

--- src/components/PostCards/index.tsx
+++ src/components/PostCards/index.tsx
@@ -6,6 +6,7 @@ import Image from '../Utils/Image';
 import Link from '../Utils/Link';
 import Summary from '../Utils/Summary';
 import TimeAgo from '../Utils/TimeAgo';
+import { cloudinaryImages } from '../../services/cloudinaryImages';
 
 
 interface PostCards {
@@ -13,14 +14,19 @@ interface PostCards {
   widthStyle?: 'long' | 'small';
 }
 
+// Original > https://res.cloudinary.com/lucasfernandodev/image/upload/v1668859449/blog/thumbnails/Deep_Catcher_kdevdv.jpg
+
+// Modify = https://res.cloudinary.com/lucasfernandodev/image/upload/c_scale,q_56,w_1052/v1668859449/blog/thumbnails/Deep_Catcher_kdevdv.webp
+
 const PostCards = ({ posts, widthStyle }: PostCards) => {
+
   return (
     <div className={style.wrapper}>
       {posts !== null &&
         posts.map((post, index: number) => (
           <div className={style.card} key={index} data-style={widthStyle}>
             <div className={style.cardHeader}>
-              <Image src={post.cover} alt={post.title} />
+              <Image src={cloudinaryImages(post.cover)} alt={post.title} />
             </div>
             <div className={style.cardMain}>
               <h3>
