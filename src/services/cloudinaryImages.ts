4003407c85948a600578fa402b537f1f26672409
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-20 10:45:04 -0300

    chore: fixing errors lint

--- src/services/cloudinaryImages.ts
+++ src/services/cloudinaryImages.ts
@@ -1,14 +1,13 @@
 // Converte imagem to low quality
 export function cloudinaryImages(url: string): string{
   const cloudName = 'https://res.cloudinary.com/lucasfernandodev/image/upload';
-  const folderpath = 'blog/thumbnails'
-  const transformation = 'c_scale,q_50,w_550'
-  const typefile = 'f_webp'
+  const folderpath = 'blog/thumbnails';
+  const transformation = 'c_scale,q_50,w_550';
+  const typefile = 'f_webp';
 
-  const urlFields = url.split("/")
-  const filename = urlFields[urlFields.length - 1]
+  const urlFields = url.split('/');
+  const filename = urlFields[urlFields.length - 1];
 
   const image = `${cloudName}/${transformation}/${typefile}/${folderpath}/${filename}`;
-  console.log("image", image)
   return image;
 };
