695fa0d2e49287cc8f49181623da4daf75a4a7ef
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-22 16:27:36 -0300

    style: improving heading color

--- src/components/interface/Molecules/PostCards/index.tsx
+++ src/components/interface/Molecules/PostCards/index.tsx
@@ -7,26 +7,29 @@ import Link from '@/infra/Link';
 import Summary from '@/Atons/Summary';
 import { cloudinaryImages } from '@/services/cloudinaryImages';
 
-
 interface PostCards {
   posts: BlogPost[];
   widthStyle?: 'long' | 'small';
 }
 
 const PostCards = ({ posts, widthStyle }: PostCards) => {
-
   return (
     <div className={style.wrapper}>
       {posts !== null &&
         posts.map((post, index: number) => (
           <div className={style.card} key={index} data-style={widthStyle}>
             <div className={style.cardHeader}>
-              <Image src={cloudinaryImages(post.cover)} alt={post.title} width={550} height={354}/>
+              <Image
+                src={cloudinaryImages(post.cover)}
+                alt={post.title}
+                width={550}
+                height={354}
+              />
             </div>
             <div className={style.cardMain}>
-              <h3>
-                <Link href={`/post/${post.slug}`}>{post.title}</Link>
-              </h3>
+              <Link href={`/post/${post.slug}`}>
+                <h3>{post.title} </h3>
+              </Link>
 
               <Summary content={post.description} />
 
