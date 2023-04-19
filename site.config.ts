7e7086512cee733f6e2da48bc56489f8e6104d71
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-04-18 22:11:47 -0300

    style: add semicolon

--- site.config.ts
+++ site.config.ts
@@ -1,5 +1,5 @@
 export const canonicalUrl = process.env.SITE_URL;
 export const sitePreview = `${canonicalUrl}/assets/preview.jpg`;
-export const siteName = 'Blog do Lucas Fernando'
-export const siteNameCompleted = 'Blog do Lucas Fernando - Um blog sobre desenvolvimento web'
-export const siteDescription = 'Um blog sobre desenvolvimento web, com publicações diárias de artigos sobre programação, css e noticias.'
+export const siteName = 'Blog do Lucas Fernando';
+export const siteNameCompleted = 'Blog do Lucas Fernando - Um blog sobre desenvolvimento web';
+export const siteDescription = 'Um blog sobre desenvolvimento web, com publicações diárias de artigos sobre programação, css e noticias.';
