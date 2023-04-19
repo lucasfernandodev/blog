d77e2fb231a04bdd9e95ab96c6fb8fd57ed69207
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-04-18 23:00:16 -0300

    style: improving site description

--- site.config.ts
+++ site.config.ts
@@ -2,4 +2,4 @@ export const canonicalUrl = process.env.SITE_URL;
 export const sitePreview = `${canonicalUrl}/assets/preview.jpg`;
 export const siteName = 'Blog do Lucas Fernando';
 export const siteNameCompleted = 'Blog do Lucas Fernando - Um blog sobre desenvolvimento web';
-export const siteDescription = 'Um blog sobre desenvolvimento web, com publicações diárias de artigos sobre programação, css e noticias.';
+export const siteDescription = 'Explore e encontre uma variedade de artigos de programação, UX e tecnologias em geral.';
