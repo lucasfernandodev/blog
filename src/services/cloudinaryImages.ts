a7dcad4b5d600e0fb800d2106e0ae82a840a4516
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-04-21 13:33:20 -0300

    feat: improving cloudinary thumbnail transformation

--- src/services/cloudinaryImages.ts
+++ src/services/cloudinaryImages.ts
@@ -1,11 +1,17 @@
 // Converte imagem to low quality
-export function cloudinaryImages(url: string): string{
-  const cloudName = 'https://res.cloudinary.com/lucasfernandodev/image/upload';
-  const folderpath = 'blog/thumbnails';
-  const transformation = 'c_scale,q_95,w_590';
-  const typefile = 'f_webp';
+export function cloudinaryImages(url: string): string {
 
   const urlFields = url.split('/');
+  const urlSplit = url.split('.');
+
+  const cloudName = 'https://res.cloudinary.com/lucasfernandodev/image/upload';
+  const folderpath = 'blog/thumbnails';
+  const transformation = 'c_scale,q_85,w_1024';
+  const fileType = urlSplit[urlSplit.length - 1];
+
+  const typefile = fileType === 'svg' ? 'f_svg' : 'f_webp';
+
+
   const filename = urlFields[urlFields.length - 1];
 
   const image = `${cloudName}/${transformation}/${typefile}/${folderpath}/${filename}`;
