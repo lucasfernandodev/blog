8439e923d0d8a5e27abbf0ebcb7199870b928522
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-04-22 14:21:53 -0300

    chore: remove image fallback

--- src/infra/Image.tsx
+++ src/infra/Image.tsx
@@ -1,15 +1,14 @@
 import NextImage from 'next/image';
-import {ImageProps} from 'next/image';
+import { ImageProps } from 'next/image';
 import { useState } from 'react';
-import imageError from '../components/Utils/imageError';
 
 interface imageProps extends ImageProps {
-  src: string,
-  alt: string,
-  width?: number,
-  height?: number,
-  className?: string,
-  priority?: boolean,
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+  className?: string;
+  priority?: boolean;
 }
 
 const Image = ({
@@ -22,6 +21,9 @@ const Image = ({
   ...args
 }: imageProps) => {
   const [currentSrc, setCurrentSrc] = useState(src);
+
+  const imageError =
+    'https://res.cloudinary.com/lucasfernandodev/image/upload/v1653322546/blog/Cover_default_fyuwgl.svg';
 
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
