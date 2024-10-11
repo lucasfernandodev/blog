f5fb3a66a74141ab9884627712c62fe229b084b1
Lucas Fernando <lucasfernando.dev@gmail.com>
2024-10-11 16:10:55 -0300

    refactor: Removing light mode functionality

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -2,11 +2,8 @@ import Image from 'next/image';
 import style from './style.module.css';
 import IconSearch from '@/assets/icons/search';
 import Link from 'next/link';
-import { ThemeSwicher } from '@/components/client/ThemeSwicher';
-import { cookies } from 'next/headers';
 
 export const Header = () => {
-  const theme = cookies().get("theme")?.value === "dark-mode" ? 'dark-mode' : 'light-mode'
 
   return (
     <header className={style.header}>
@@ -19,7 +16,6 @@ export const Header = () => {
           <IconSearch />
         </button>
       </form>
-    <ThemeSwicher theme={theme}/>
     </header>
   )
 }
